57b3b20e10dc462726ec4a8227dad89fbe577cd2
Luis Bastidas <luisbastir@gmail.com>
2019-04-07 00:29:53 -0600

    hapi node

---
+++ node/hapi/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var Hapi = require('hapi');
+var app = Hapi.server({
+  host: 'localhost',
+  port: 3000
+});
+
+// Simple JSON test
+app.route({
+  method:'GET',
+  path:'/helloworld',
+  handler: function(request, h) {
+    return {
+      message: 'Hello World!'
+    };
+  }
+});
+
+// Simple list with string interpolation
+app.route({
+  method:'GET',
+  path:'/list',
+  handler: function(request, h) {
+    var list = [];
+    for (var x = 0; x < 100; x++) {
+      var item = {
+        index: x,
+        message: 'Item ' + (x + 1) + ' with index ' + x
+      };
+      list.push(item);
+    }
+    return list;
+  }
+});
+
+// Simple Fibonacci List
+function fibonacci(num) {
+	var sequence = [1, 1];
+	for (var i = 2; i < num; i++) {
+		sequence[i] = sequence[i-1]+ sequence[i-2];
+	}
+	return sequence[num-1];
+}
+app.route({
+  method:'GET',
+  path:'/fibonacci',
+  handler: function(request, h) {
+    var list = [];
+    for (var x = 0; x < 100; x++) {
+      list.push(fibonacci(x));
+    }
+    return list;
+  }
+});
+
+const start = async function() {
+  try {
+    app.start().then(function() {
+      // success
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+start();
